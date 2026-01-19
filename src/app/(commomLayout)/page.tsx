225371dd279da6852efbfb3c684519152f43cbf3
Fajla Rabby <fajlarabbyla24@gmail.com>
2026-01-19 12:03:49 +0600

    32-11 Authentication with Google OAuth

--- src/app/(commomLayout)/page.tsx
+++ src/app/(commomLayout)/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "../../components/ui/button";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div>
       <Button variant={"default"}>Click Here!</Button>
--- src/app/(commomLayout)/sign-up/page.tsx
+++
@@ -1,5 +0,0 @@
-const SignUpPage = () => {
-  return <div>This is sign up page</div>;
-};
-
-export default SignUpPage;
