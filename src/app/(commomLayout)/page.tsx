d9bfef733c97332ff1a0844997c2eff0a184853b
FajlaRabby24 <fajlarabbyla24@gmail.com>
2026-01-27 18:52:58 +0600

    33-1 getSession data in server component using fetch

--- src/app/(commomLayout)/page.tsx
+++ src/app/(commomLayout)/page.tsx
@@ -1,6 +1,20 @@
+import { cookies } from "next/headers";
 import { Button } from "../../components/ui/button";
 
 export default async function Home() {
+  const cookieStore = await cookies();
+  // console.log(cookieStore.get("__next_hmr_refresh_hash__"));
+
+  const res = await fetch(`http://localhost:5000/api/auth/get-session`, {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+    cache: "no-store",
+  });
+
+  const session = await res.json();
+  console.log(session);
+
   return (
     <div>
       <Button variant={"default"}>Click Here!</Button>
